9c2cdedca19c902f4de0988284fa51481bb6f4e5
Joe Sweeney <jsween2303@gmail.com>
2026-04-24 12:01:39 -0700

    using sbatch instead of srun

---
+++ run_angsd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=angsd
+#SBATCH --partition=bmh
+#SBATCH --account=millermrgrp
+#SBATCH --time=08:00:00
+#SBATCH --mem=256G
+#SBATCH --cpus-per-task=12
+
+module load angsd
+angsd -nthreads 12 \
+-minInd 364 -bam bamlists/fil.bamlist -out outputs/101.2/snps-fil \
+-ref /home/jsweeney/newark/genomes/genome.fasta \
+-minMaf 0.05 -minMapQ 20 -minQ 20 -GL 1 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 \
+-doGeno 2 -doPost 1 -postCutoff 0.95 -doPlink 2 > outputs/101.2/snps-fil.out 2> outputs/101.2/snps-fil.err
