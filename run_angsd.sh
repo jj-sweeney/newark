97c15f53e213b066771dc4f83bb83ac8fd742cac
Joe Sweeney <jsween2303@gmail.com>
2026-04-28 13:24:41 -0700

    editing batch script directly instead of assigning variables

--- run_angsd.sh
+++ run_angsd.sh
@@ -6,8 +6,9 @@
 #SBATCH --mem=256G
 #SBATCH --cpus-per-task=12
 
+module load angsd
 angsd -nthreads 12 \
--minInd $min_ind -bam bamlists/$bamlist -out outputs/101.2/$output \
+-minInd 184 -bam bamlists/$bamlist -out outputs/101.2/snps-nwrk_west \
 -ref /home/jsweeney/newark/genomes/genome.fasta \
 -minMaf 0.05 -minMapQ 20 -minQ 20 -GL 1 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 \
--doGeno 2 -doPost 1 -postCutoff 0.95 -doPlink 2 > outputs/101.2/$output.out 2> outputs/101.2/$output.err
+-doGeno 2 -doPost 1 -postCutoff 0.95 -doPlink 2 > outputs/101.2/snps-nwrk_west.out 2> outputs/101.2/snps-nwrk_west.err
