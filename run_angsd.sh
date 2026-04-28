d25b104538bef7526437cb18a03b3283928c5ae5
Joe Sweeney <jsween2303@gmail.com>
2026-04-28 13:01:26 -0700

    snp discovery on western cluster

--- run_angsd.sh
+++ run_angsd.sh
@@ -8,7 +8,7 @@
 
 module load angsd
 angsd -nthreads 12 \
--minInd $min_ind -bam bamlists/nwrk.bamlist -out outputs/101.2/snps-nwrk \
+-minInd $min_ind -bam bamlists/$bamlist -out outputs/101.2/$output \
 -ref /home/jsweeney/newark/genomes/genome.fasta \
 -minMaf 0.05 -minMapQ 20 -minQ 20 -GL 1 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 \
--doGeno 2 -doPost 1 -postCutoff 0.95 -doPlink 2 > outputs/101.2/snps-fil.out 2> outputs/101.2/snps-nwrk.err
+-doGeno 2 -doPost 1 -postCutoff 0.95 -doPlink 2 > outputs/101.2/$output.out 2> outputs/101.2/$output.err
