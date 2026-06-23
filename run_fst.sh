11de14042b4fe9e9e54e7c62d29f810b44b7ee33
Joe Sweeney <jsween2303@gmail.com>
2026-06-23 13:29:41 -0700

    Calculating Fst

--- run_fst.sh
+++ run_fst.sh
@@ -4,8 +4,6 @@
 #SBATCH --account=millermrgrp
 #SBATCH --time=8:00:00
 #SBATCH --mem=64G
-#SBATCH --output=fst_%j.out
-#SBATCH --error=fst_%j.err
 
 module load vcftools
 
@@ -14,5 +12,16 @@ vcftools --vcf outputs/101.2/pruned-nwrk.vcf --weir-fst-pop poplists/ColdCreekRe
 	--weir-fst-pop poplists/NN3_Mid.txt \
 	--weir-fst-pop poplists/NN3_North.txt \
 	--weir-fst-pop poplists/NN4.txt \
+	--weir-fst-pop poplists/NV1.txt \
+	--weir-fst-pop poplists/NV2.txt \
+	--weir-fst-pop poplists/NV5.txt \
+	--weir-fst-pop poplists/NV6.txt \
+	--weir-fst-pop poplists/NV7.txt \
+	--weir-fst-pop poplists/NV9.txt \
+	--weir-fst-pop poplists/NV12.txt \
+	--weir-fst-pop poplists/NV16.txt \
+	--weir-fst-pop poplists/NV17.txt \
+	--weir-fst-pop poplists/NV18.txt \
+	--weir-fst-pop poplists/NV19.txt \
 	--out outputs/105/
 	
