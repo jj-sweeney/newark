ec3b0bbdae2093494d43e86841bc5c37863c56ba
Joe Sweeney <jsween2303@gmail.com>
2026-06-23 13:06:07 -0700

    Calculate Fst

--- run_fst.sh
+++ run_fst.sh
@@ -8,7 +8,9 @@
 module load vcftools
 
 vcftools pruned-nwrk.vcf --weir-fst-pop poplists/ColdCreekReservoir.txt \
-	--weir-fst-pop poplists/NF6.TXT
-	--weir-fst-pop poplists/NN3_Mid.txt
-	--weir-fst-pop poplists/NN3_North.txt
-	--NN4.txt
+	--weir-fst-pop poplists/NF6.TXT \
+	--weir-fst-pop poplists/NN3_Mid.txt \
+	--weir-fst-pop poplists/NN3_North.txt \
+	--NN4.txt \
+	--out outputs/105
+	
